e31f13776313525a489514ef8f01fa1859c30794
Pietro <prebi94@gmail.com>
2018-01-27 08:16:14 +0100

    pistola che cambia peso pseudo funzionante

--- Assets/Props/grate/Grate.cs
+++ Assets/Props/grate/Grate.cs
@@ -3,13 +3,22 @@ using UnityEngine;
 
 public class Grate : MonoBehaviour
 {
+    public float maxFloatingPointY;
+    bool grabbed;
 
     private void OnTriggerEnter (Collider other)
     { 
         if(other.gameObject.tag == "crate")
         {
-
             StartCoroutine (RoutineAlignCrate (other.gameObject.transform));
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "crate")
+        {
+            StartCoroutine(ElevateCrate(other.gameObject.transform));
         }
     }
 
@@ -19,12 +28,43 @@ public class Grate : MonoBehaviour
         Vector3 startPos = crate.position;
         Vector3 endPos = new Vector3 (transform.position.x, crate.position.y, transform.position.z);
         float t = 0;
-        while(t < 1)
+        while(t < 1 && !grabbed)
         {
             crate.position = Vector3.Lerp (startPos, endPos, t);
             t += Time.deltaTime;
             yield return new WaitForEndOfFrame ();
 
         }
+        grabbed = true;
+    }
+
+    IEnumerator ElevateCrate(Transform crate)
+    {
+        Color currentColor = crate.GetComponent<Renderer>().material.color;
+        float y;
+
+        if(currentColor.Equals(Color.white))
+        {
+            y = 0.4f;
+        }
+        else
+        {
+            y = maxFloatingPointY;
+        }
+        if(grabbed)
+        {
+            crate.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            Vector3 startPos = crate.position;
+            Vector3 endPos = new Vector3(crate.position.x, y, crate.position.z);
+            float t = 0;
+            while (t < 1)
+            {
+                crate.position = Vector3.Lerp(startPos, endPos, t);
+                t += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+
+            }
+        }
+        yield return null;
     }
 }
