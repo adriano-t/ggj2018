238730756859c27f1de90ca85ff0641f774f44de
Pietro <prebi94@gmail.com>
2018-01-27 17:03:39 +0100

    asd

---
+++ Assets/Cisterna.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cisterna : MonoBehaviour {
+
+    Color finalColor = new Color(1f, 0.3103447f, 0f);
+
+    public void IncreaseEmission()
+    {
+        Color current = GetComponent<Renderer>().material.GetColor("_EmissionColor");
+        Color nextColor = Color.Lerp(current, finalColor,Time.deltaTime);
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", nextColor);
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,11 +15,11 @@ public class PlayerController : MonoBehaviour
 
     public ParticleSystem ps;
     //sounds
-    [SerializeField] 
+    [SerializeField]
     AudioClip[] soundSteps;
-    AudioSource sndSourceSteps; 
+    AudioSource sndSourceSteps;
     bool soundStepsCanPlay;
-    
+
     public Transform cam;
     private Rigidbody rb;
     private CapsuleCollider cc;
@@ -30,60 +30,56 @@ public class PlayerController : MonoBehaviour
     Vector3 directionVector;
     float bobbing = 0;
     float breathing = 0;
-    bool crouch = false; 
+    bool crouch = false;
     float camCrouch = 0;
     private bool jump;
 
-    LayerMask crateLayerMask;
-    LayerMask fireLayerMask;
     public GameObject gun;
 
-    void Awake ()
+    void Awake()
     {
-        cam = GetComponentInChildren<Camera> ().transform;
-        rb = GetComponent<Rigidbody> ();
-        cc = GetComponent<CapsuleCollider> ();
-        crateLayerMask = LayerMask.GetMask("crate");
-        fireLayerMask = LayerMask.GetMask("fire");
+        cam = GetComponentInChildren<Camera>().transform;
+        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CapsuleCollider>();
         //sounds
-        sndSourceSteps = gameObject.AddComponent<AudioSource> ();
+        sndSourceSteps = gameObject.AddComponent<AudioSource>();
         sndSourceSteps.clip = soundSteps[0];
         sndSourceSteps.volume = 0.3f;
 
- 
+
         camLocalStart = cam.localPosition;
 
         speed = walkSpeed;
     }
 
-    void Start ()
+    void Start()
     {
-        
+
     }
 
 
-    void Update ()
+    void Update()
     {
-      
+
         //rotate body
         //Vector3 ang = body.localEulerAngles;
         //Quaternion handTargetRot = Quaternion.Euler (new Vector3 (ang.x, cam.transform.eulerAngles.y, ang.z));
         //body.rotation = Quaternion.Slerp (body.rotation, handTargetRot, 0.5f);
 
 
-        directionVector = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+        directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         float bobbingY = 0;
         if (directionVector != Vector3.zero)
         {
 
-            if (Input.GetKey (KeyCode.LeftShift))
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                speed = Mathf.Lerp (speed, runSpeed, 0.1f);
+                speed = Mathf.Lerp(speed, runSpeed, 0.1f);
             }
             else
             {
-                speed = Mathf.Lerp (speed, walkSpeed, 0.4f);
+                speed = Mathf.Lerp(speed, walkSpeed, 0.4f);
             }
             if (crouch)
             {
@@ -94,9 +90,9 @@ public class PlayerController : MonoBehaviour
 
             Transform camTransform = cam;
             float euler = camTransform.localEulerAngles.x;
-            camTransform.Rotate (new Vector3 (-euler, 0, 0));
+            camTransform.Rotate(new Vector3(-euler, 0, 0));
             directionVector = camTransform.rotation * directionVector;
-            camTransform.Rotate (new Vector3 (euler, 0, 0));
+            camTransform.Rotate(new Vector3(euler, 0, 0));
 
             //if (!controller.isGrounded)
             //{
@@ -105,7 +101,7 @@ public class PlayerController : MonoBehaviour
 
             //controller.Move (directionVector.normalized * speed * Time.deltaTime); old but gold
 
-            if (Vector3.Angle (directionVector, rb.velocity) > 5)
+            if (Vector3.Angle(directionVector, rb.velocity) > 5)
             {
                 speedMultiplyer = 2f;
                 bobbingSpeedMultiplyer = 4;
@@ -119,21 +115,21 @@ public class PlayerController : MonoBehaviour
             float rbSpeed = rb.velocity.magnitude / speedMultiplyer;
             float ratio = (rbSpeed - walkSpeed) / (runSpeed - walkSpeed) + 0.1f;
 
-            bobbingY = (Mathf.Cos (bobbing * Mathf.PI / 180f) * bobbingAmount) / (7f - ratio);
+            bobbingY = (Mathf.Cos(bobbing * Mathf.PI / 180f) * bobbingAmount) / (7f - ratio);
             bobbing += (rbSpeed * (5f - ratio)) * bobbingSpeed * Time.deltaTime * bobbingSpeedMultiplyer;
             if (bobbingY < 0)
             {
                 if (soundStepsCanPlay)
                 {
-                    if(soundSteps.Length > 0)
-                        sndSourceSteps.clip = soundSteps[Random.Range (0, soundSteps.Length)];
+                    if (soundSteps.Length > 0)
+                        sndSourceSteps.clip = soundSteps[Random.Range(0, soundSteps.Length)];
                     else
-                        sndSourceSteps.clip = soundSteps[Random.Range (0, soundSteps.Length)];
+                        sndSourceSteps.clip = soundSteps[Random.Range(0, soundSteps.Length)];
 
                     sndSourceSteps.volume = (speed / runSpeed) * (speed / runSpeed) * 0.3f;
                     sndSourceSteps.pitch = 0.95f + 0.1f * Random.value;
-                    sndSourceSteps.Play ();
-                    soundStepsCanPlay = false; 
+                    sndSourceSteps.Play();
+                    soundStepsCanPlay = false;
                 }
             }
             else
@@ -145,9 +141,9 @@ public class PlayerController : MonoBehaviour
         {
             speed = 0;
         }
-         
 
-        if (Input.GetKey (KeyCode.LeftControl))
+
+        if (Input.GetKey(KeyCode.LeftControl))
         {
             crouch = true;
         }
@@ -161,24 +157,24 @@ public class PlayerController : MonoBehaviour
 
         if (crouch)
         {
-            camCrouch = Mathf.Lerp (camCrouch, -0.9f, 0.2f);
+            camCrouch = Mathf.Lerp(camCrouch, -0.9f, 0.2f);
             cc.height = 0.6f;
-            cc.center = new Vector3 (0, 0.38f, 0);
+            cc.center = new Vector3(0, 0.38f, 0);
         }
         else
         {
-            camCrouch = Mathf.Lerp (camCrouch, 0, 0.2f);
+            camCrouch = Mathf.Lerp(camCrouch, 0, 0.2f);
             cc.height = 1.7f;
-            cc.center = new Vector3 (0, 0.85f, 0);
+            cc.center = new Vector3(0, 0.85f, 0);
         }
 
         breathing += (-30.0f + Random.value * 150.0f) * Time.deltaTime;
-        float breathingY = 0.02f * Mathf.Cos (breathing * Mathf.PI / 180f);
+        float breathingY = 0.02f * Mathf.Cos(breathing * Mathf.PI / 180f);
 
-        cam.localPosition = new Vector3 (camLocalStart.x, camLocalStart.y + bobbingY + camCrouch + breathingY, camLocalStart.z);
+        cam.localPosition = new Vector3(camLocalStart.x, camLocalStart.y + bobbingY + camCrouch + breathingY, camLocalStart.z);
 
 
-        if (Input.GetKeyDown (KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             /*if (Physics.Raycast (transform.position, Vector3.down, 0.5f,Layers.Negate (Layers.MASK_PLAYER)))
             {
@@ -189,82 +185,81 @@ public class PlayerController : MonoBehaviour
 
         //leva peso
         RaycastHit hitInfo;
-        if (Input.GetMouseButton(1) && Physics.Raycast (cam.position, cam.transform.forward, out hitInfo, 5f,crateLayerMask))
-        {
-            string tag = hitInfo.collider.tag;
-            if(tag == "crate")
-            {
-                if(hitInfo.transform.position.y<4.5f)
-                {
-                    hitInfo.transform.Translate(Vector3.up * Time.deltaTime *2f);
-                    weightAmmo += 0.01f;
-                    SetGunParticles (hitInfo.point, gun.transform.position);
-                }
-            }
-        }
-        //aggiungi peso 
-        else if (Input.GetMouseButton(0) && Physics.Raycast(cam.position, cam.transform.forward, out hitInfo, 5f, crateLayerMask))
+        if (Input.GetMouseButton(0) && Physics.Raycast(cam.position, cam.transform.forward, out hitInfo, 5f))
         {
             string tag = hitInfo.collider.tag;
             if (tag == "crate")
             {
-                if (hitInfo.transform.position.y > 0.5f && weightAmmo>0)
+                if (hitInfo.transform.position.y < 4.5f)
                 {
-                    hitInfo.transform.Translate(Vector3.down * Time.deltaTime * 2f);
-                    weightAmmo -= 0.01f;
-                    SetGunParticles (gun.transform.position, hitInfo.point);
+                    hitInfo.transform.Translate(Vector3.up * Time.deltaTime * 2f);
+                    weightAmmo += 0.01f;
+                    SetGunParticles(hitInfo.point, gun.transform.position);
                 }
             }
-        }
-
-        //Assorbo il fuoco
-        if(Input.GetMouseButton(0) && Physics.Raycast(cam.position, cam.transform.forward, out hitInfo, 5f, fireLayerMask))
-        {
-            string tag = hitInfo.collider.tag;
-            if ( tag == "fire" && !hitInfo.transform.GetComponent<Fire>().dead)
+            else if (tag == "fire" && !hitInfo.transform.GetComponent<Fire>().dead)
             {
                 hitInfo.transform.GetComponent<Fire>().DecreaseFire();
-                fireAmmo++;
+                fireAmmo += 0.5f;
                 SetGunParticles(hitInfo.point, gun.transform.position);
+            }
+            else if (tag == "cisterna" && fireAmmo > 0)
+            {
+                hitInfo.transform.GetComponent<Cisterna>().IncreaseEmission();
             }
             else
             {
                 ps.gameObject.SetActive(false);
             }
         }
+        //aggiungi peso 
+        else if (Input.GetMouseButton(1) && Physics.Raycast(cam.position, cam.transform.forward, out hitInfo, 5f))
+        {
+            string tag = hitInfo.collider.tag;
+            if (tag == "crate")
+            {
+                if (hitInfo.transform.position.y > 0.5f && weightAmmo > 0)
+                {
+                    hitInfo.transform.Translate(Vector3.down * Time.deltaTime * 2f);
+                    weightAmmo -= 0.01f;
+                    SetGunParticles(gun.transform.position, hitInfo.point);
+                }
+            }
+        }
         else
         {
-            ps.gameObject.SetActive (false);
+            ps.gameObject.SetActive(false);
         }
+
     }
 
     void SetGunParticles(Vector3 startPos, Vector3 endPos)
     {
-        ps.gameObject.SetActive (true);
+        ps.gameObject.SetActive(true);
         ps.transform.position = startPos;
         ps.transform.forward = (endPos - startPos).normalized;
     }
-     
-    void FixedUpdate ()
+
+    void FixedUpdate()
     {
-        MovePhysics (directionVector.normalized); 
-        if(jump)
+        MovePhysics(directionVector.normalized);
+        if (jump)
         {
-            jump = false; 
-            rb.AddForce (Vector3.up * 5.5f, ForceMode.VelocityChange);
+            jump = false;
+            rb.AddForce(Vector3.up * 5.5f, ForceMode.VelocityChange);
         }
     }
 
-    void MovePhysics (Vector3 direction)
-    { 
+    void MovePhysics(Vector3 direction)
+    {
         Vector3 targetVelocity = direction * (speed / speedMultiplyer);
         targetVelocity.y = rb.velocity.y;
 
         Vector3 velocityChange = (targetVelocity - rb.velocity);
-        
-        rb.AddForce (velocityChange, ForceMode.VelocityChange);
+
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
     }
-     
-     
-    
+
+
+
 }
