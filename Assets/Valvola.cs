4dc715af1bc49f12a774e2eb8738bb63a1c77ca8
Pietro <prebi94@gmail.com>
2018-01-27 23:02:45 +0100

    ciao

---
+++ Assets/Valvola.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Valvola : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            GetComponent<Animator>().SetBool("turn", true);
+        }
+    }
+
+}
