222b6a8d36a52c0a335f1164600d7c769bf2d0b2
Pietro <prebi94@gmail.com>
2018-01-28 01:13:55 +0100

    asd

--- Assets/Cisterna.cs
+++ Assets/Cisterna.cs
@@ -6,16 +6,24 @@ public class Cisterna : MonoBehaviour {
 
     Color finalColor = new Color(1f, 0.3103447f, 0f);
     Light glow;
+    int c = 0;
+
 
     public void Start()
     {
         glow = GetComponentInChildren<Light>();
     }
+
     public void IncreaseEmission()
     {
+        c++;
         Color current = GetComponent<Renderer>().material.GetColor("_EmissionColor");
         Color nextColor = Color.Lerp(current, finalColor,Time.deltaTime*0.5f);
         glow.intensity = Mathf.Lerp(glow.intensity, 5f,Time.deltaTime*0.25f);
         GetComponent<Renderer>().material.SetColor("_EmissionColor", nextColor);
+        if(c==325)
+        {
+            Grate.active = true;
+        }
     }
 }
