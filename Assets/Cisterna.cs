4dc2d3a237250489a3cbda8e701a25780ad50c3d
Adriano Tumminelli <adriano.tummi@hotmail.it>
2018-01-29 11:40:13 +0100

    cisterna

--- Assets/Cisterna.cs
+++ Assets/Cisterna.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Cisterna : MonoBehaviour {
 
-    Color finalColor = new Color(1f, 0.3103447f, 0f);
+    Color startGlowColor = new Color (0.5f, 0.1f, 0f);
+    Color finalColor = new Color (1f, 0.3103447f, 0f);
     Light glow;
     float c = 0;
     float max = 390;
@@ -16,13 +17,14 @@ public class Cisterna : MonoBehaviour {
         GetComponent<AudioSource>().clip = boilFx;
         GetComponent<AudioSource>().volume = 0.2f;
         GetComponent<AudioSource>().Play();
+        startGlowColor = GetComponent<Renderer> ().material.GetColor ("_EmissionColor");
     }
 
     public void IncreaseEmission()
     {
         c++;
         GetComponent<AudioSource>().volume = c / max ;
-        GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.Lerp (Color.black, finalColor, c / max));
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.Lerp (startGlowColor, finalColor, c / max));
         if(c/max > 0.5f)
         { 
             glow.range = Mathf.Lerp (1, 5, (c/max - 0.5f) * 2 );
